a4a48d617b0d0b166291dadb62707fb8dbd19cbf
Alex Auritt <alexauritt@yahoo.com>
2013-03-26 14:44:40 -0400

    Initial Commit

---
+++ NerdFeed/NerdfeedAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NerdfeedAppDelegate.h
+//  NerdFeed
+//
+//  Created by Alexander Auritt on 3/26/13.
+//  Copyright (c) 2013 Alexander Auritt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NerdfeedAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NerdFeed/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NerdFeed
+//
+//  Created by Alexander Auritt on 3/26/13.
+//  Copyright (c) 2013 Alexander Auritt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NerdfeedAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([NerdfeedAppDelegate class]));
+  }
+}
